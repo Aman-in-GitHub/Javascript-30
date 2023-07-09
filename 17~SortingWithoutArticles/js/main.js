6a97175fb13f736ed784585fa027899380cfe0f3
Aman <121707291+Aman-in-GitHub@users.noreply.github.com>
2023-07-09 23:35:30 +0545

    Add files via upload

---
+++ 17~SortingWithoutArticles/js/main.js
@@ -0,0 +1,66 @@
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
+
+function strip(bandName) {
+  return bandName.replace(/^(a |the |an )/i, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => {
+  return strip(a) > strip(b) ? 1 : -1;
+});
+
+document.querySelector('#bands').innerHTML = sortedBands
+  .map((band) => `<li>${band}</li>`)
+  .join('');
+
+const light = document.querySelector('.light');
+const dark = document.querySelector('.dark');
+
+const userTheme = localStorage.getItem('theme');
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const themeCheck = () => {
+  if (userTheme == 'dark' || (!userTheme && systemTheme)) {
+    document.documentElement.classList.add('dark');
+    light.classList.remove('hidden');
+    return;
+  }
+  dark.classList.remove('hidden');
+};
+
+const themeSwitch = () => {
+  if (document.documentElement.classList.contains('dark')) {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+    light.classList.add('hidden');
+    dark.classList.remove('hidden');
+    return;
+  }
+  document.documentElement.classList.add('dark');
+  localStorage.setItem('theme', 'dark');
+  dark.classList.add('hidden');
+  light.classList.remove('hidden');
+};
+
+dark.addEventListener('click', () => {
+  themeSwitch();
+});
+
+light.addEventListener('click', () => {
+  themeSwitch();
+});
+
+themeCheck();

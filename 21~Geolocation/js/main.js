ab4ea719cee44f4553d79da7e4744be93b2f4454
Aman <121707291+Aman-in-GitHub@users.noreply.github.com>
2023-07-10 19:36:23 +0545

    Add files via upload

---
+++ 21~Geolocation/js/main.js
@@ -0,0 +1,52 @@
+const arrow = document.querySelector('.arrow');
+const speed = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition(
+  (data) => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.error(err);
+  }
+);
+
+const light = document.querySelector('.light');
+const dark = document.querySelector('.dark');
+
+const userTheme = localStorage.getItem('theme');
+const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+const themeCheck = () => {
+  if (userTheme == 'dark' || (!userTheme && systemTheme)) {
+    document.documentElement.classList.add('dark');
+    light.classList.remove('hidden');
+    return;
+  }
+  dark.classList.remove('hidden');
+};
+
+const themeSwitch = () => {
+  if (document.documentElement.classList.contains('dark')) {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+    light.classList.add('hidden');
+    dark.classList.remove('hidden');
+    return;
+  }
+  document.documentElement.classList.add('dark');
+  localStorage.setItem('theme', 'dark');
+  dark.classList.add('hidden');
+  light.classList.remove('hidden');
+};
+
+dark.addEventListener('click', () => {
+  themeSwitch();
+});
+
+light.addEventListener('click', () => {
+  themeSwitch();
+});
+
+themeCheck();
